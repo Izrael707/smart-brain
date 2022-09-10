c7a916ed59d98b5819e5c0532aaac6e9ba8ea0b5
izrael707 <dkingisreal@gmail.com>
2022-09-10 20:02:34 +0100

    edits

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,8 +5,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        This Magic Brain will detect faces in your pictures. Git it a try.
       </p>
+      <p className='f4'>Copy and paste the image-address of the picture you want to detect faces in.</p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
